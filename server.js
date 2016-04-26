4f9f06b97d2569adab814fc4fc1315404c5d247d
ivchenkodima <ivchenkodima@gmail.com>
2016-04-26 15:41:07 +0300

    fix

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var http = require('http');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 
-var serve = serveStatic("./");
+var serve = serveStatic("./build");
 
 var server = http.createServer(function(req, res) {
     var done = finalhandler(req, res);
     serve(req, res, done);
 });
 
-server.listen(80);
+server.listen(8000);
